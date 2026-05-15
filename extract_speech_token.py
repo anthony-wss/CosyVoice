bf8f07f3fa712ddd986610ff55dd8c6cd6e2c32c
Anthony Wang <r13921059@ntu.edu.tw>
2026-05-15 18:11:16 +0800

    Implement CosyVoice 1

--- extract_speech_token.py
+++ extract_speech_token.py
@@ -53,28 +53,84 @@ class CosyVoice3Tokenizer:
         return speech_token, speech_token_len
 
 
+class CosyVoiceTokenizer:
+    def __init__(self):
+        model_dir='pretrained_models/CosyVoice-300M'
+        hyper_yaml_path = '{}/cosyvoice.yaml'.format(model_dir)
+        with open(hyper_yaml_path, 'r') as f:
+            configs = load_hyperpyyaml(f)
+        self.frontend = CosyVoiceFrontEnd(configs['get_tokenizer'],
+                                          configs['feat_extractor'],
+                                          '{}/campplus.onnx'.format(model_dir),
+                                          '{}/speech_tokenizer_v1.onnx'.format(model_dir),
+                                          '{}/spk2info.pt'.format(model_dir),
+                                          configs['allowed_special'])
+        self.sample_rate = configs['sample_rate']
+        self.device = "cuda"
+        del configs
+    
+    def extract_token(self, path=None, wave=None, sr=None):
+        if path:
+            speech = load_wav(path, 16000)
+        elif wave is not None:
+            # We assume wave shape is [N] here
+            assert sr, "you must provide the sampling rate"
+            speech = torch.from_numpy(wave)
+            if sr != 16000:
+                assert sr >= 16000, f'wav sample rate {sr} must be greater than 16000'
+                speech = torchaudio.transforms.Resample(orig_freq=sr, new_freq=16000)(speech)
+            speech = speech.view([1, -1])
+        else:
+            raise ValueError("You must provide either path or wave data")
+
+        assert speech.shape[1] / 16000 <= 30, 'do not support extract speech token for audio longer than 30s'
+        feat = whisper.log_mel_spectrogram(speech, n_mels=128)
+        speech_token = self.frontend.speech_tokenizer_session.run(None,
+                                                         {self.frontend.speech_tokenizer_session.get_inputs()[0].name:
+                                                          feat.detach().cpu().numpy(),
+                                                          self.frontend.speech_tokenizer_session.get_inputs()[1].name:
+                                                          np.array([feat.shape[2]], dtype=np.int32)})[0].flatten().tolist()
+        speech_token = torch.tensor([speech_token], dtype=torch.int32).to(self.device)
+        speech_token_len = torch.tensor([speech_token.shape[1]], dtype=torch.int32).to(self.device)
+        return speech_token, speech_token_len
+
+
 def main(args):
-    tokenizer = CosyVoice3Tokenizer()
+    ver = args.cv_version
+    if ver == "1":
+        tokenizer = CosyVoiceTokenizer()
+    elif ver == "2":
+        raise NotImplementedError("We do not implement CosyVoice2 yet")
+    elif ver == "3":
+        tokenizer = CosyVoice3Tokenizer()
+    else:
+        raise ValueError(f"cv_version should be 1, 2, or 3")
+
     ds = load_from_disk(args.hf_dataset)
 
     # Debug
-    ds = ds.select(range(5))
+    # ds = ds.select(range(5))
 
-    def extract_CV3_token(sample):
-        if "model_res_token_cv3" in sample:
+    def extract_CV_token(sample):
+        if f"model_res_token_cv{ver}" in sample:
             return sample
         model_res_audio = sample["model_res_audio"]
-        speech_token, _ = tokenizer.extract_token(wave=model_res_audio["array"], sr=model_res_audio["sampling_rate"])
-        speech_token = speech_token.tolist()
-        sample["model_res_token_cv3"] = speech_token
+        try:
+            speech_token, _ = tokenizer.extract_token(wave=model_res_audio["array"], sr=model_res_audio["sampling_rate"])
+            speech_token = speech_token.tolist()
+        except Exception as e:
+            print(f"Error while extracting CosyVoice {ver} token: {e}")
+            speech_token = []
+        sample[f"model_res_token_cv{ver}"] = speech_token
         return sample
 
-    ds = ds.map(extract_CV3_token)
+    ds = ds.map(extract_CV_token)
     # ds.save_to_disk(args.hf_dataset)
     ds.save_to_disk("_debug_hf_dataset")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("hf_dataset", help="path to the source huggingface dataset", type=str)
+    parser.add_argument("--cv_version", help="CosyVoice version. Should be 1, 2, or 3", type=str)
     args = parser.parse_args()
     main(args)
--- token2wav.py
+++ token2wav.py
@@ -6,36 +6,69 @@ import json
 import uuid
 import torch
 import soundfile as sf
+import argparse
 
 # AUDIO_PROMPT_PATH="/home/anthony/CosyVoice/zero_shot_0.wav"
 # OUTPUT_PATH="/home/anthony/CosyVoice/output.wav"
 AUDIO_PROMPT_PATH="/work/u3937558/seedvc/ref_speech.mp3"
 OUTPUT_PATH="./output.wav"
 
-def main():
-    cosyvoice = AutoModel(model_dir='pretrained_models/Fun-CosyVoice3-0.5B')
+def main(args):
+    if args.cv_version == "1":
+        cosyvoice = AutoModel(model_dir='pretrained_models/CosyVoice-300M')
+    elif args.cv_version == "2":
+        raise NotImplementedError("We do not implement CosyVoice2 yet")
+    elif args.cv_version == "3":
+        cosyvoice = AutoModel(model_dir='pretrained_models/Fun-CosyVoice3-0.5B')
+    else:
+        raise ValueError(f"cv_version should be 1, 2, or 3")
 
-    with open("speech_tokens_cv3.json", "r") as f:
+    with open("speech_tokens_cv1.json", "r") as f:
         speech_token = json.load(f)["speech_token"]
         speech_token = torch.tensor(speech_token)
+
     flow_prompt_speech_token, _ = cosyvoice.frontend._extract_speech_token(AUDIO_PROMPT_PATH)
     prompt_speech_feat, _ = cosyvoice.frontend._extract_speech_feat(AUDIO_PROMPT_PATH)
     flow_embedding = cosyvoice.frontend._extract_spk_embedding(AUDIO_PROMPT_PATH)
     this_uuid = str(uuid.uuid1())
     speed = 1.0
     
-    output_wav = cosyvoice.model.token2wav(
-        token=speech_token,
-        prompt_token=flow_prompt_speech_token,
-        prompt_feat=prompt_speech_feat,
-        embedding=flow_embedding,
-        token_offset=0,
-        uuid=this_uuid,
-        stream=False,
-        finalize=True,
-        speed=speed
-    )
+    if args.cv_version == "1":
+        with cosyvoice.model.lock:
+            cosyvoice.model.tts_speech_token_dict[this_uuid], cosyvoice.model.llm_end_dict[this_uuid] = [], False
+            cosyvoice.model.hift_cache_dict[this_uuid] = None
+            cosyvoice.model.mel_overlap_dict[this_uuid] = torch.zeros(1, 80, 0)
+            cosyvoice.model.flow_cache_dict[this_uuid] = torch.zeros(1, 80, 0, 2)
+        output_wav = cosyvoice.model.token2wav(
+            token=speech_token,
+            prompt_token=flow_prompt_speech_token,
+            prompt_feat=prompt_speech_feat,
+            embedding=flow_embedding,
+            uuid=this_uuid,
+            finalize=True,
+            speed=speed
+        )
+    elif args.cv_version == "2":
+        raise NotImplementedError("We do not implement CosyVoice2 yet")
+    elif args.cv_version == "3":
+        output_wav = cosyvoice.model.token2wav(
+            token=speech_token,
+            prompt_token=flow_prompt_speech_token,
+            prompt_feat=prompt_speech_feat,
+            embedding=flow_embedding,
+            token_offset=0,
+            uuid=this_uuid,
+            stream=False,
+            finalize=True,
+            speed=speed
+        )
+    else:
+        raise ValueError(f"cv_version should be 1, 2, or 3")
+    
     sf.write(OUTPUT_PATH, output_wav.squeeze().cpu().numpy(), 24000)
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cv_version", help="CosyVoice version. Should be 1, 2, or 3", type=str)
+    args = parser.parse_args()
+    main(args)
